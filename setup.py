28250aa7af9a18f856e5fde8115b7736e4a5e8d9
smithykitty <catherinemckernansmith@gmail.com>
2025-04-20 12:17:47 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='Montecarlo Simulator',
+     version='1.0',
+     url='',
+     description='Monte Carlo Simulator Module with Die, Game, and Analyzer',
+     author = 'Catherine Smith',
+     author_email = 'cmj4ec@virginia.edu',
+     license='MIT',
+     packages=['montecarlosim'],
+     install_requires=['numpy', 'pandas'])
